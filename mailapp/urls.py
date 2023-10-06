30c75ead3cd8ba3babef24f8a974c2826faee42e
Sergey <smirsand@mail.ru>
2023-10-06 21:37:52 +0300

    add code urls.py mailapp

--- mailapp/urls.py
+++ mailapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from mailapp.apps import MailappConfig
-from mailapp.views import NewsletterListView
+from mailapp.views import NewsletterListView, NewsletterCreateView, NewsletterUpdateView
 
 app_name = MailappConfig.name
 
 urlpatterns = [
-    path('mailapp_list', NewsletterListView.as_view(), name='mailapp_list')
+    path('mailapp_list', NewsletterListView.as_view(), name='mailapp_list'),
+    path('create', NewsletterCreateView.as_view(), name='create_newsletter'),
+    # path('update/<int:pk>/', NewsletterUpdateView.as_view(), name='mailapp_form')
 ]
