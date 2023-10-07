6f13abe4040458fa0a8bcc53f70e7c957fe87a58
Sergey <smirsand@mail.ru>
2023-10-07 19:13:13 +0300

    add migrations

--- message/migrations/0002_alter_mailinglog_options.py
+++ message/migrations/0002_alter_mailinglog_options.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('mailapp', '0001_initial'),
+        ('message', '0001_initial'),
     ]
 
     operations = [
