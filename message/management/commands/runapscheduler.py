d5d3434e5edc9f8fcfc779dc17fcab54201552e9
Sergey <smirsand@mail.ru>
2023-10-11 23:19:43 +0300

    add code runapscheduler.py

--- message/management/commands/runapscheduler.py
+++ message/management/commands/runapscheduler.py
@@ -1,44 +1,17 @@
-# runapscheduler.py
 import logging
 
 from django.conf import settings
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
-from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 from django_apscheduler import util
 
-from message.models import MailingLog, Newsletter
+from message.services import send_mails
 
 logger = logging.getLogger(__name__)
-
-from django.core.mail import send_mail
-
-
-def my_job():
-    newsletters = Newsletter.objects.all()
-    logs = []  # Создание пустого списка для хранения журналов
-    for newsletter in newsletters:
-        subject = newsletter.message.subject
-        content = newsletter.message.content
-        emails = newsletter.clients.all().values_list('email', flat=True)  # Получение списка email всех клиентов
-
-        for email in emails:
-            send_mail(
-                subject,
-                content,
-                settings.EMAIL_HOST_USER,
-                [email],
-                fail_silently=False
-            )
-            log = MailingLog(status=MailingLog.STATUS_OK, newsletter=newsletter)  # Создание журнала внутри цикла
-            log.save()  # Сохранение журнала
-            logs.append(log)  # Добавление журнала в список logs
-
-    return logs  # Возвращение списка logs, содержащего все журналы
 
 
 # The `close_old_connections` decorator ensures that database connections, that have become
@@ -65,13 +38,13 @@ class Command(BaseCommand):
         scheduler.add_jobstore(DjangoJobStore(), "default")
 
         scheduler.add_job(
-            my_job,
-            trigger=CronTrigger(second="*/10"),  # Every 10 seconds
-            id="my_job",  # The `id` assigned to each job MUST be unique
+            send_mails,
+            trigger=CronTrigger(second="*/20"),  # Every 10 seconds
+            id="send_mails",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
         )
-        logger.info("Added job 'my_job'.")
+        logger.info("Added job 'send_mails'.")
 
         scheduler.add_job(
             delete_old_job_executions,
