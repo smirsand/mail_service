672bc8b307e04e02cce54a61f798cd3297e30ba4
Sergey <smirsand@mail.ru>
2023-10-12 19:25:21 +0300

    add code urls.py users

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView, generate_new_password
+from users.views import RegisterView, ProfileView, generate_new_password, pass_verification
 
 app_name = UsersConfig.name
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('profile/genpassword/', generate_new_password, name='generate_new_password'),
+    path('verification/<int:pk>', pass_verification, name='verification'),
 ]
