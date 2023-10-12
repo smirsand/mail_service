de223909209956d4b08e97146048fdd2cd97e8b7
Sergey <smirsand@mail.ru>
2023-10-12 19:25:39 +0300

    add code views.py users

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 import random
 
-from django.shortcuts import redirect
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, get_object_or_404, render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView
 
-from message.services import send_new_password
-from users.forms import UserRegisterForm, UserProfileForm
+from config import settings
+from message.services import send_new_password, generate_confirmation_password, send_for_confirmation
+from users.forms import UserRegisterForm, UserProfileForm, UserVerificationForm
 from users.models import User
 
 
@@ -15,13 +18,25 @@ class RegisterView(CreateView):
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
 
+    def get_success_url(self):
+        return reverse('users:verification', args=[self.object.pk])
+
+    def form_valid(self, form):
+        new_user = form.save()
+        new_user.code = generate_confirmation_password()
+        new_user.save()
+
+        send_for_confirmation(new_user.pk)
+
+        return super().form_valid(form)
+
 
 class ProfileView(UpdateView):
     model = User
     form_class = UserProfileForm
     success_url = reverse_lazy('users:profile')
 
-    def get_object(self, queryset=None):
+    def get_object(self, queryset=None):  # Для редактирования текущего пользователя.
         return self.request.user
 
 
@@ -31,3 +46,21 @@ def generate_new_password(request):
     request.user.save()
     send_new_password(request.user.email, new_password)
     return redirect(reverse('client:list_client'))
+
+
+def pass_verification(request, pk):
+    user = get_object_or_404(User, pk=pk)
+
+    if request.method == 'POST':
+        form = UserVerificationForm(request.POST)
+        user_code = request.POST.get('code')
+
+        if user.code == user_code:
+            user.is_active = True
+            user.save()
+            return redirect(reverse('users:login'))
+
+    else:
+        form = UserVerificationForm()
+
+    return render(request, 'users/verification_form.html', {'form': form, 'pk': pk})
