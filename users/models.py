48aab9f49b5d58ceeb3591e9c0785d1c89fd1b57
Sergey <smirsand@mail.ru>
2023-10-13 22:33:30 +0300

    add files users

--- users/models.py
+++ users/models.py
@@ -9,6 +9,14 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='телефон', blank=True, null=True)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', blank=True, null=True)
     code = models.CharField(max_length=12, verbose_name='проверочный код', blank=True, null=True)
+    is_active = models.BooleanField(default=True, verbose_name='активирован')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        permissions = [
+            ('is_active', 'Can block user')
+        ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, get_object_or_404, render
@@ -31,7 +32,7 @@ class RegisterView(CreateView):
         return super().form_valid(form)
 
 
-class ProfileView(UpdateView):
+class ProfileView(LoginRequiredMixin, UpdateView):
     model = User
     form_class = UserProfileForm
     success_url = reverse_lazy('users:profile')
