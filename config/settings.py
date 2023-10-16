71008bce4fcc23b975842458281f0d78dfee96f0
Sergey <smirsand@mail.ru>
2023-10-16 15:50:38 +0300

    add code settings.py

--- config/settings.py
+++ config/settings.py
@@ -158,6 +158,6 @@ if CACHE_ENABLED:
     CACHES = {
         "default": {
             "BACKEND": "django.core.cache.backends.redis.RedisCache",
-            "LOCATION": "redis://127.0.0.1:6379",
+            "LOCATION": os.getenv('CACHES_LOCATION')
         }
     }
